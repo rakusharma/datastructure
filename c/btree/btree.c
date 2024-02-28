97f267a45a03e95e6c9bde7fe7d65a9e210b0df5
Ravi Sharma <Ravi.Sharma@analog.com>
2024-02-28 16:14:53 +0000

    pre post and inorder

--- c/btree/btree.c
+++ c/btree/btree.c
@@ -26,6 +26,13 @@ struct item *push(void **p, struct item **root)
     return x;
 }
 
+struct item *pop(struct item **root)
+{
+    struct item *t = *root;
+    *root = (*root)->next;
+    return t;
+}
+
 
 int stack_len(struct item *top)
 {
@@ -35,29 +42,20 @@ int stack_len(struct item *top)
         ++n;
         i = i->next;
     }
-
-    printf("stack len %4d\n", n);
-
+    //printf("stack len %4d\n", n);
     return n;
-
 }
 void print_stack(struct item *top)
 {
     struct item *i = top;
+
     printf("*stack -> %p\n ", i);
     while (i != NULL) {
-        printf("ptr -> %p val ->%d\n", i->ptr, ((struct node*)(i->ptr))->d);
+       // printf("ptr -> %p val ->%d\n", i->ptr, ((struct node*)(i->ptr))->d);
+        printf("item val ->%d ", ((struct node*)(i->ptr))->d);
         i = i->next;
     }
-    printf("*stack ->ends \n ");
-}
-struct item *pop(struct item **root)
-{
-    struct item *t = *root;
-
-    *root = (*root)->next;
-
-    return t;
+    printf("\nstack ->endsi\n");
 }
 
 
@@ -70,29 +68,29 @@ struct node *alloc(int d)
     return t;
 }
 
-struct node *in(int d, struct node *node)
+struct node *add_node(int d, struct node *node)
 {
     if (node == NULL)
         return alloc(d);
     else if (d > node->d) 
-        node->r = in(d, node->r);
+        node->r = add_node(d, node->r);
     else if (d < node->d)
-        node->l = in(d, node->l); 
-
+        node->l = add_node(d, node->l); 
     return node;
 }
 
 /*in order print*/
-void print_io(struct node *root)
+void dfs_in_order(struct node *root)
 {
     if(root == NULL) return;
     // stack = push(root, stack);
-    print_io(root->l);
-    printf(" :%d: ", root->d);
-    print_io(root->r);
+    dfs_in_order(root->l);
+    printf(" %d ", root->d);
+    dfs_in_order(root->r);
 }
 
-void *in_nr(int d, struct node **root)
+/*non recursive in*/
+void *add_node_nr(int d, struct node **root)
 {
 
     struct node **cur = root;
@@ -105,12 +103,14 @@ void *in_nr(int d, struct node **root)
     }
 }
 
-void print_io_nr(struct node *root)
+void dfs_in_order_nr(struct node *root)
 {
     struct node *cur = root;
     struct item *stack = NULL, *item;
     int *p[100] = {0x00};
     int i = 0;    
+
+    printf("\ndfs in order non recursive => ");
 
     while(1) {
         if (cur != NULL) {
@@ -126,23 +126,182 @@ void print_io_nr(struct node *root)
         }
     }
     for (int x = 0; x < 10; ++x) 
-        p[x] != NULL ? printf(" :%d : ", ((struct node*)p[x])->d): (x = x);
+        p[x] != NULL ? printf(" %d  ", ((struct node*)p[x])->d): (x = x);
+}
 
+
+/*
+ * pushing stack and analyzing does not work here
+void dfs_in_order_nr(struct node *root)
+{
+    int *visit[100] = {0x00};
+    struct item *stack = NULL, *item;
+    struct node *cur = root;
+    int i = 0;
+
+    stack = push(&cur, &stack);
+
+    while (stack_len(stack) != 0) {
+        cur = pop(&stack);
+        if (cur->l == NULL && cur->r == NULL) {
+            visit[i++] = cur;
+        }
+        if (cur->l) {
+            stack = push(&cur, &stack);
+            stack = push(&(cur->l), &stack);
+        }
+        if (cur->r) {
+            stack = push(&cur, &stack);
+            stack = push(&(cur->r), &stack);
+        }
+
+
+
+    }
+
+
+
+}
+*/
+void dfs_pre_order_nr(struct node *root)
+{
+    int *visit[100] = {0x00};
+    struct item *stack = NULL, *item;
+    struct node *cur = root;
+    int i = 0;
+    stack = push(&cur, &stack);
+    //visit[i++] = cur;
+     //print_stack(stack);
+
+    while(stack_len(stack) != 0) {
+
+        item = pop(&stack);
+        cur = (struct node*)(item->ptr);
+        visit[i++] = cur;
+
+        if(cur->r)stack = push(&(cur->r), &stack);
+        if(cur->l)stack = push(&(cur->l), &stack);
+
+        //print_stack(stack);
+    }
+
+    printf("\npreoder node -> ");
+    for (int j = 0; j < i; ++j)
+        printf(" %2d ", ((struct node*)visit[j])->d);
+
+
+}
+
+struct _item {
+   void *ptr;
+   int visited;
+   struct _item *next;
+};
+struct _item *_push(void **p, struct item **root, int visited)
+{
+    struct _item *x = malloc(sizeof(struct item));
+    x->ptr = *p;
+    x->visited = visited;
+    if(*root != NULL){
+        x->next = *root;
+        return x;
+    }
+    x->next = NULL;
+    return x;
+}
+
+struct item *_pop(struct _item **root)
+{
+    struct _item *t = *root;
+    *root = (*root)->next;
+    return t;
+}
+
+
+int _stack_len(struct _item *top)
+{
+    struct _item *i = top;
+    int n = 0;
+    while (i != NULL) {
+        ++n;
+        i = i->next;
+    }
+    //printf("stack len %4d\n", n);
+    return n;
+}
+void _print_stack(struct _item *top)
+{
+    struct _item *i = top;
+
+    printf("*stack -> %p\n ", i);
+    while (i != NULL) {
+       // printf("ptr -> %p val ->%d\n", i->ptr, ((struct node*)(i->ptr))->d);
+        printf("item val ->%d ", ((struct node*)(i->ptr))->d);
+        i = i->next;
+    }
+    printf("\nstack ->ends\n");
+}
+
+
+
+/*1->2, 1->3  => 2, 3, 1*/
+
+#define TRUE 1
+#define FALSE 0
+void dfs_post_order_nr(struct node *root)
+{
+    int *visit[100] = {0x00};
+    struct _item *stack = NULL, *item;
+    struct node *cur = root;
+    int i = 0;
+    stack = _push(&cur, &stack, FALSE);
+
+
+    while(_stack_len(stack) != 0) {
+        item =_pop(&stack);
+        cur = (struct node*)(item->ptr);
+        if(item->visited) {
+        visit[i++] = cur;
+        } 
+        else {
+            stack = _push(&cur, &stack, TRUE);
+
+           if (cur->r)  stack = _push(&(cur->r), &stack, FALSE);
+            if (cur->l) stack = _push(&(cur->l), &stack, FALSE);
+        }
+
+
+
+   }
+
+    printf("dfs post order -> ");
+    for (int j = 0; j < i; ++j)
+        printf(" %2d ", ((struct node*)visit[j])->d);
+
+ 
+ 
 }
 
 
 int main()
 {
     struct node *root = NULL;
-    in_nr(4, &root);
 
-    in_nr(6, &root);
-    in_nr(8, &root);
-    in_nr(12, &root);
-    in_nr(15, &root);
+    add_node_nr(8, &root);
+    add_node_nr(3, &root);
+    add_node_nr(10, &root);
+    add_node_nr(1, &root);
+    add_node_nr(6, &root);
+    add_node_nr(4, &root);
+    add_node_nr(7, &root);
+    add_node_nr(14, &root);
+    add_node_nr(13, &root);
 
-    print_io(root);
-    print_io_nr(root);
+    printf("dfs in order recursive  => ");
+    dfs_in_order(root);
+    dfs_in_order_nr(root);
+    dfs_pre_order_nr(root);
+    dfs_post_order_nr(root);
     //print_stack(stack);
 
     return 0;
---
+++ c/btree/readme.md
@@ -0,0 +1,8 @@
+Unlike linear data structures (Array, Linked List, Queues, Stacks, etc) which have only one logical way to traverse them, trees can be traversed in different ways. 
+
+A Tree Data Structure can be traversed in following ways:
+
+Depth First Search or DFS
+-   Inorder Traversal => Traverse left, visit root, Traverse right  
+-   Preorder Traversal => Visit root, traverse left, traverse right
+-   Postorder Traversal => Traverse left, Traverse Right, Visit root
